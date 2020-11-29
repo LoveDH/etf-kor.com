e67659c7cd721b390a97a0329e7c7b6f46177a03
LoveDH <dhyeok1996@gmail.com>
2020-11-29 03:49:53 +0000

    세계 지수 트렌드 업데이트

---
+++ lib/getfromDB.py
@@ -0,0 +1,264 @@
+import pymysql
+from sqlalchemy import create_engine
+import json
+import pandas as pd
+
+import dash
+import dash_table
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import plotly.express as px
+
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+HOSTNAME = config["MYSQL_INFO"]['HOSTNAME']
+PORT     = config["MYSQL_INFO"]['PORT']
+USERNAME = config["MYSQL_INFO"]['USERNAME']
+PASSWORD = config["MYSQL_INFO"]['PASSWORD']
+DATABASE = config["MYSQL_INFO"]['DATABASE']
+CHARSET1 = config["MYSQL_INFO"]['CHARSET1']    # MySQL에서 사용할 캐릭터셋 이름
+CHARSET2 = config["MYSQL_INFO"]['CHARSET2']    # Python에서 사용할 캐릭터셋 이름
+
+indices = {'KS11':'코스피','KQ11':'코스닥','US500':'S&P500'}
+
+connection = pymysql.connect(
+    host=HOSTNAME,
+    user=USERNAME,
+    password=PASSWORD,
+    db=DATABASE,
+    charset=CHARSET1)
+
+cursor = connection.cursor()
+sql = "select Symbol,Name from etfkor.etfList"
+cursor.execute(sql)
+etflist = pd.DataFrame(cursor.fetchall(), columns=['Symbol','Name'])
+
+def get_indice_data(name,index_period=30):
+    connection = pymysql.connect(
+    host=HOSTNAME,
+    user=USERNAME,
+    password=PASSWORD,
+    db=DATABASE,
+    charset=CHARSET1)
+
+    cursor = connection.cursor()
+    sql = "select * from etfkor."+name
+    cursor.execute(sql)
+    try:
+        index_period = -index_period
+    except:
+        index_period = -30
+    df = pd.DataFrame(cursor.fetchall()[index_period:], columns=['Date', 'Close', 'Open', 'High', 'Low', 'Volume', 'Change'])
+    cursor.close()
+    
+    fig = go.Figure(data=[go.Candlestick(x=df['Date'],
+                open=df['Open'],
+                high=df['High'],
+                low=df['Low'],
+                close=df['Close'],increasing_line_color= 'Red', decreasing_line_color= 'Blue')],
+                layout={'height':200,'width':370,'margin':{"r":0,"t":0,"l":0,"b":0}})
+    fig.update_layout(xaxis=dict(visible=False,type='category',rangeslider=dict(visible=False)))
+    fig.update_traces(line={'width':1}, selector=dict(type='candlestick'))
+    
+    return fig
+
+def get_map_data():
+    connection = pymysql.connect(
+        host=HOSTNAME,
+        user=USERNAME,
+        password=PASSWORD,
+        db=DATABASE,
+        charset=CHARSET1)
+
+    cursor = connection.cursor()
+    sql = "select Name, yield_from_ex, market_cap, type from etfkor.etfList"
+    cursor.execute(sql)
+
+    df = pd.DataFrame(cursor.fetchall(), columns=['종목명','등락률','시가총액','유형'])
+    cursor.close()
+
+    fig = px.treemap(df, path=['유형','종목명'], values='시가총액',
+                    color='등락률', hover_data=['종목명'],
+                    color_continuous_scale='RdBu_r',
+                    color_continuous_midpoint=0)
+    fig.update_layout({'margin':{"r":10,"t":0,"l":0,"b":10}})
+
+    connection = pymysql.connect(
+        host=HOSTNAME,
+        user=USERNAME,
+        password=PASSWORD,
+        db=DATABASE,
+        charset=CHARSET1)
+
+    cursor = connection.cursor()
+    sql = """SELECT * FROM etfkor.indices WHERE Symbol IN 
+    ('^GSPC','^FTSE','^GDAXI','^FCHI','IMOEX.ME','^N225','^HSI','000001.SS','^STI','^AXJO','^BSESN','^JKSE','^KLSE','^NZ50','^KS11','^GSPTSE','^BVSP',
+    '^MXX','^IPSA','^MERV','^TA125.TA','^CASE30')"""
+    cursor.execute(sql)
+
+    df = pd.DataFrame(cursor.fetchall(), columns=['코드','지수명','국가','국가코드','현재가','전일대비','등락률'])
+    cursor.close()
+    df['등락률'] = df['등락률'].str[:-1].astype(float)
+    df['pn'] = ['상승' if i > 0 else '하락' for i in df['등락률']]
+    df['등락률'] = abs(df['등락률'])
+    fig2 = px.scatter_geo(df, locations="국가코드", color="pn",
+                        hover_name="국가", size=df['등락률'],size_max=60,
+                        hover_data=['지수명','현재가','전일대비'],
+                        color_discrete_sequence=['red','blue'],
+                        projection="robinson")
+    fig2.update_layout({'margin':{"r":10,"t":0,"l":0,"b":10}})
+    result_html = html.Div([
+        dcc.Graph(
+            id='ETF유형별 등락률',
+            figure=fig
+        ),
+        dcc.Graph(
+            id='월드맵',
+            figure=fig2
+        )
+    ])
+
+    return result_html
+
+
+def get_etf_table_by_market_cap(by):
+
+    connection = pymysql.connect(
+        host=HOSTNAME,
+        user=USERNAME,
+        password=PASSWORD,
+        db=DATABASE,
+        charset=CHARSET1)
+    sql = "select Name, now_price, yield_from_ex, market_cap from etfkor.etfList"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    df = pd.DataFrame(cursor.fetchall(), columns=['종목명','현재가','등락률','시가총액'])
+    if by == 'mc':
+        df.sort_values(by='시가총액', ascending=False, inplace=True)
+        df = df.head(20)
+    elif by == 'yield_top':
+        df.sort_values(by='등락률', ascending=False, inplace=True)
+        df = df.head(10)
+    elif by == 'yield_bottom':
+        df.sort_values(by='등락률', ascending=True, inplace=True)
+        df = df.head(10)
+
+    result = dash_table.DataTable(
+        data=df.to_dict('records'),
+        columns=[{'id': c, 'name': c} for c in df.columns],
+        style_as_list_view=True,
+        style_cell={'fontFamily': 'sans-serif', 'fontSize': '11px','padding':0},
+        style_data={'border':'0px'},
+        style_header={
+            'backgroundColor': 'white',
+            'fontWeight': 'bold'
+        },
+        style_cell_conditional=[            
+            {'if': {'column_id': '종목명'},'textAlign': 'left','width': '200px'},
+            {'if': {'column_id': '시가총액'},'width': '100px'},
+            {'if': {'column_id': '등락률'},'width': '100px'}
+        ],
+        style_data_conditional=[
+            {
+                'if': {
+                'filter_query': '{등락률} > 0',
+                'column_id': '등락률'
+                },
+                'color': 'Red'
+            },
+            {
+                'if': {
+                'filter_query': '{등락률} < 0',
+                'column_id': '등락률'
+                },
+                'color': 'Blue'
+            },
+        ]
+    )
+
+    return result
+
+def get_mini_treemap():
+
+    connection = pymysql.connect(
+        host=HOSTNAME,
+        user=USERNAME,
+        password=PASSWORD,
+        db=DATABASE,
+        charset=CHARSET1)
+    cursor = connection.cursor()
+    sql = "select Name, yield_from_ex, market_cap, type from etfkor.etfList"
+    cursor.execute(sql)
+
+    df = pd.DataFrame(cursor.fetchall(), columns=['종목명','등락률','시가총액','유형'])
+    cursor.close()
+
+    fig = px.treemap(df, path=['유형','종목명'], values='시가총액',
+                    color='등락률', hover_data=['종목명'],
+                    color_continuous_scale='RdBu_r',
+                    color_continuous_midpoint=0)
+    fig.update_layout({'margin':{"r":0,"t":0,"l":0,"b":00},'width':400,'height':300})
+    fig.update(layout_coloraxis_showscale=False)
+    result = dcc.Graph(
+            id='ETF유형별 등락률',
+            figure=fig
+        )
+    return result
+
+def get_etf_pie_chart():
+
+    connection = pymysql.connect(
+        host=HOSTNAME,
+        user=USERNAME,
+        password=PASSWORD,
+        db=DATABASE,
+        charset=CHARSET1)
+    cursor = connection.cursor()
+    sql = "select Name, market_cap from etfkor.etfList"
+    cursor.execute(sql)
+
+    df = pd.DataFrame(cursor.fetchall(), columns=['종목명','시가총액'])
+    cursor.close()
+
+    df = df.head(50)
+    fig = px.pie(df, values='시가총액', names='종목명')
+    fig.update_layout({'title':'시장 비율','margin':{"r":0,"t":50,"l":0,"b":10},'width':350,'height':350})
+    fig.update_traces(hole=.4,textposition='inside', textinfo='percent+label',showlegend=False)
+    result = dcc.Graph(
+            id='ETF 시장 비율',
+            figure=fig
+    )
+    
+    return result
+
+def get_etf_single_chart(code):
+    connection = pymysql.connect(
+        host=HOSTNAME,
+        user=USERNAME,
+        password=PASSWORD,
+        db=DATABASE,
+        charset=CHARSET1)
+
+    cursor = connection.cursor()
+    sql = "select * from etfkor."+code
+    cursor.execute(sql)
+    df = pd.DataFrame(cursor.fetchall(), columns=['Date', 'Close', 'Open', 'High', 'Low', 'Volume', 'Change'])
+
+    cursor.close()
+
+    fig = go.Figure(data=[go.Candlestick(x=df['Date'],
+                open=df['Open'],
+                high=df['High'],
+                low=df['Low'],
+                close=df['Close'],increasing_line_color= 'Red', decreasing_line_color= 'Blue')],
+                layout={'height':200,'width':370,'margin':{"r":0,"t":0,"l":0,"b":0}})
+    fig.update_layout(xaxis=dict(visible=False,type='category',rangeslider=dict(visible=False)))
+    fig.update_traces(line={'width':1}, selector=dict(type='candlestick'))
+
+
+    result = dcc.Graph(figure=fig)
+
+    return result
---
+++ lib/update_data.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import json
+
+import pymysql
+from sqlalchemy import create_engine
+
+with open('../config.json', 'r') as f:
+    config = json.load(f)
+
+HOSTNAME = config["MYSQL_INFO"]['HOSTNAME']
+PORT     = config["MYSQL_INFO"]['PORT']
+USERNAME = config["MYSQL_INFO"]['USERNAME']
+PASSWORD = config["MYSQL_INFO"]['PASSWORD']
+DATABASE = config["MYSQL_INFO"]['DATABASE']
+CHARSET1 = config["MYSQL_INFO"]['CHARSET1']    # MySQL에서 사용할 캐릭터셋 이름
+CHARSET2 = config["MYSQL_INFO"]['CHARSET2']    # Python에서 사용할 캐릭터셋 이름
+
+con_str_fmt = "mysql+mysqldb://{0}:{1}@{2}:{3}/{4}?charset={5}"
+con_str = con_str_fmt.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE, CHARSET1)
+pymysql.install_as_MySQLdb()
+
+# mysql과 연결
+import MySQLdb
+engine = create_engine(con_str, encoding=CHARSET2)
+conn = engine.connect()
+print('...데이터베이스 연결 완료')
+
+df_list = pd.read_html('https://finance.yahoo.com/world-indices/')
+majorStockIdx = df_list[0][:-1]
+country_code = ['USA','USA','USA','USA','USA','GBR','USA','USA','GBR','DEU',
+'FRA','EU','EU','BEL','RUS','JPY','HKG','CHN','CHN','SGP',
+'AUS','AUS','IND','IDN','MYS','NZL','KOR','CHN','CAN','BRA','MEX','ESP','ARG','ISR','EGY']
+country_dict = {'USA':'미국','GBR':'영국','DEU':'벨기에','FRA':'프랑스','EU':'유럽','BEL':'벨기에','RUS':'러시아',
+                'JPY':'일본','HKG':'홍콩','CHN':'중국','SGP':'싱가포르','AUS':'호주','IND':'인도','IDN':'인도네시아',
+                'MYS':'말레이시아','NZL':'뉴질랜드','KOR':'대한민국','CAN':'캐나다','BRA':'브라질','MEX':'멕시코',
+                'ESP':'스페인','ARG':'아르헨티나','ISR':'이스라엘','EGY':'이집트'}
+majorStockIdx['Code'] = country_code
+majorStockIdx['Country'] = majorStockIdx['Code'].map(country_dict)
+majorStockIdx = majorStockIdx[['Symbol','Name','Country','Code','Last Price','Change','% Change']]
+
+
+majorStockIdx.to_sql(name='indices', con=conn, if_exists='replace', index=False)
