ad52da02406a3fc8c715045ccf5a316bd16983de
LoveDH <dhyeok1996@gmail.com>
2020-12-18 08:46:41 +0000

    코드 배치 및 mini tree map 호버 데이터 변경

--- lib/getfromDB.py
+++ lib/getfromDB.py
@@ -69,6 +69,99 @@ def get_indice_data(name,index_period=30):
     
     return fig
 
+# HOME 데이터 테이블 호출
+def get_etf_table_by_market_cap(by):
+
+    sql = "select Name, now_price, yield_from_ex, market_cap from etfkor.etfList"
+    df = pd.DataFrame(get_data_from_db(sql), columns=['종목명','현재가','등락률','시가총액'])
+
+    if by == '시가총액 TOP 20':
+        df.sort_values(by='시가총액', ascending=False, inplace=True)
+        df = df.head(20)
+    elif by == '등락률 TOP 10':
+        df.sort_values(by='등락률', ascending=False, inplace=True)
+        df = df.head(10)
+    elif by == '등락률 BOTTOM 10':
+        df.sort_values(by='등락률', ascending=True, inplace=True)
+        df = df.head(10)
+
+    # 데이터 정리
+    df['현재가'] = df['현재가'].apply(lambda x : "{:,}".format(int(x)))
+    df['등락률'] = df['등락률'].apply(lambda x : "+"+str(x) if x>0 else str(x))
+    df['시가총액'] = df['시가총액'].apply(lambda x : "{:,}".format(int(x/100000000)))
+    df.rename(columns={'종목명':by,'현재가':'현재가(원)','등락률':'등락률(%)','시가총액':'시가총액(억원)'}, inplace=True)
+
+    result = dash_table.DataTable(
+        data=df.to_dict('records'),
+        columns=[{'id': c, 'name': c} for c in df.columns],
+        style_as_list_view=True,
+        style_cell={'fontFamily': 'sans-serif', 'fontSize': '11px','padding':0},
+        style_data={'border':'0px'},
+        style_header={
+            'backgroundColor': 'rgb(230, 230, 230)',
+            'fontWeight': 'bold'
+        },
+        style_cell_conditional=[            
+            {'if': {'column_id': by},'textAlign': 'left','width': '200px'},
+            {'if': {'column_id': '시가총액'},'width': '100px'},
+            {'if': {'column_id': '등락률'},'width': '100px'}
+        ],
+        style_data_conditional=[
+            {
+                'if': {
+                'filter_query': '{등락률(%)} contains +',
+                'column_id': '등락률(%)'
+                },
+                'color': 'Red'
+            },
+            {
+                'if': {
+                'filter_query': '{등락률(%)} contains -',
+                'column_id': '등락률(%)'
+                },
+                'color': 'Blue'
+            },
+        ]
+    )
+
+    return result
+
+# HOME 미니맵 호출
+def get_mini_treemap():
+
+    sql = "select Name, yield_from_ex, market_cap, type1, type2 from etfkor.etfList"
+    df = pd.DataFrame(get_data_from_db(sql), columns=['종목명','등락률','시가총액','유형1','유형2'])
+
+    fig = px.treemap(df, path=['유형1','유형2','종목명'], values='시가총액',
+                    color='등락률', hover_data=['종목명'],
+                    color_continuous_scale='RdBu_r',
+                    color_continuous_midpoint=0)
+    fig.update_layout({'margin':{"r":0,"t":20,"l":0,"b":10},'width':400,'height':300})
+    fig.update(layout_coloraxis_showscale=False)
+    fig.update_traces(textposition='middle center', selector=dict(type='treemap'))
+    fig.data[0].hovertemplate='%{label}'
+    result = dcc.Graph(
+            id='ETF유형별 등락률',
+            figure=fig,
+            config={'staticPlot':True}
+        )
+    return result
+
+# HOME pie차트 호출
+def get_etf_pie_chart_top10():
+    sql = "select Name, market_cap from etfkor.etfList"
+
+    df = pd.DataFrame(get_data_from_db(sql), columns=['종목명','시가총액'])
+    df = df.head(10)
+    fig = px.pie(df, values='시가총액', names='종목명')
+    fig.update_layout({'title':'시장 비율','margin':{"r":0,"t":50,"l":0,"b":10},'width':350,'height':350})
+    fig.update_traces(hole=.4,textposition='inside', textinfo='percent+label',showlegend=False)
+    result = dcc.Graph(
+            id='ETF 시장 비율',
+            figure=fig
+    )
+    return result
+
 # Trends ETF 트리맵 호출
 def get_tree_map():
 
@@ -104,6 +197,7 @@ def get_world_map():
 
     return fig
 
+# World 세계지수 데이터 테이블 호출
 def get_world_table():
 
     sql = "SELECT * FROM etfkor.indices"
@@ -182,97 +276,7 @@ def get_world_table():
 
     return table_left, table_right
 
-# HOME 데이터 테이블 호출
-def get_etf_table_by_market_cap(by):
 
-    sql = "select Name, now_price, yield_from_ex, market_cap from etfkor.etfList"
-    df = pd.DataFrame(get_data_from_db(sql), columns=['종목명','현재가','등락률','시가총액'])
-
-    if by == '시가총액 TOP 20':
-        df.sort_values(by='시가총액', ascending=False, inplace=True)
-        df = df.head(20)
-    elif by == '등락률 TOP 10':
-        df.sort_values(by='등락률', ascending=False, inplace=True)
-        df = df.head(10)
-    elif by == '등락률 BOTTOM 10':
-        df.sort_values(by='등락률', ascending=True, inplace=True)
-        df = df.head(10)
-
-    # 데이터 정리
-    df['현재가'] = df['현재가'].apply(lambda x : "{:,}".format(int(x)))
-    df['등락률'] = df['등락률'].apply(lambda x : "+"+str(x) if x>0 else str(x))
-    df['시가총액'] = df['시가총액'].apply(lambda x : "{:,}".format(int(x/100000000)))
-    df.rename(columns={'종목명':by,'현재가':'현재가(원)','등락률':'등락률(%)','시가총액':'시가총액(억원)'}, inplace=True)
-
-    result = dash_table.DataTable(
-        data=df.to_dict('records'),
-        columns=[{'id': c, 'name': c} for c in df.columns],
-        style_as_list_view=True,
-        style_cell={'fontFamily': 'sans-serif', 'fontSize': '11px','padding':0},
-        style_data={'border':'0px'},
-        style_header={
-            'backgroundColor': 'rgb(230, 230, 230)',
-            'fontWeight': 'bold'
-        },
-        style_cell_conditional=[            
-            {'if': {'column_id': by},'textAlign': 'left','width': '200px'},
-            {'if': {'column_id': '시가총액'},'width': '100px'},
-            {'if': {'column_id': '등락률'},'width': '100px'}
-        ],
-        style_data_conditional=[
-            {
-                'if': {
-                'filter_query': '{등락률(%)} contains +',
-                'column_id': '등락률(%)'
-                },
-                'color': 'Red'
-            },
-            {
-                'if': {
-                'filter_query': '{등락률(%)} contains -',
-                'column_id': '등락률(%)'
-                },
-                'color': 'Blue'
-            },
-        ]
-    )
-
-    return result
-
-# HOME 미니맵 호출
-def get_mini_treemap():
-
-    sql = "select Name, yield_from_ex, market_cap, type1, type2 from etfkor.etfList"
-    df = pd.DataFrame(get_data_from_db(sql), columns=['종목명','등락률','시가총액','유형1','유형2'])
-
-    fig = px.treemap(df, path=['유형1','유형2','종목명'], values='시가총액',
-                    color='등락률', hover_data=['종목명'],
-                    color_continuous_scale='RdBu_r',
-                    color_continuous_midpoint=0)
-    fig.update_layout({'margin':{"r":0,"t":10,"l":0,"b":10},'width':400,'height':300})
-    fig.update(layout_coloraxis_showscale=False)
-    fig.update_traces(textposition='middle center', selector=dict(type='treemap'))
-    result = dcc.Graph(
-            id='ETF유형별 등락률',
-            figure=fig,
-            config={'staticPlot':True}
-        )
-    return result
-
-# HOME pie차트 호출
-def get_etf_pie_chart_top10():
-    sql = "select Name, market_cap from etfkor.etfList"
-
-    df = pd.DataFrame(get_data_from_db(sql), columns=['종목명','시가총액'])
-    df = df.head(10)
-    fig = px.pie(df, values='시가총액', names='종목명')
-    fig.update_layout({'title':'시장 비율','margin':{"r":0,"t":50,"l":0,"b":10},'width':350,'height':350})
-    fig.update_traces(hole=.4,textposition='inside', textinfo='percent+label',showlegend=False)
-    result = dcc.Graph(
-            id='ETF 시장 비율',
-            figure=fig
-    )
-    return result
 
 # Search etf 차트 호출
 def get_etf_chart(symbol, period):
@@ -376,6 +380,51 @@ def get_etf_pie_chart(symbol):
     )
     return pf_table, result
 
+#Search etf history 호출
+def get_history_table(symbol):
+    sql = "select 날짜, NAV, 시가, 고가, 저가, 종가, 거래량, 거래대금 from etfkor."+symbol
+    df = pd.DataFrame(get_data_from_db(sql),columns=['날짜','NAV','시가','고가','저가','종가','거래량','거래대금'])
+    df['날짜'] = df['날짜'].apply(lambda x: x.strftime('%Y-%m-%d'))
+    for col in ['시가','고가','저가','종가','거래량','거래대금']:
+        df[col] =df[col].apply(lambda x : "{:,}".format(int(x)))
+    df.sort_values('날짜',ascending=False,inplace=True)
+    history = dash_table.DataTable(
+        data=df.to_dict('records'),
+        columns=[{'id': c, 'name': c} for c in df.columns],
+        style_as_list_view=True,
+        page_size=10,
+        style_cell={'fontFamily': 'sans-serif', 'fontSize': '11px','padding':0},
+        style_data={'border':'0px'},
+        style_header={
+            'backgroundColor': 'rgb(230, 230, 230)',
+            'fontWeight': 'bold'
+        },
+        style_cell_conditional=[            
+            {'if': {'column_id': '날짜'},'textAlign': 'left','width': '200px'},
+            {'if': {'column_id': '시가총액'},'width': '100px'},
+            {'if': {'column_id': '등락률'},'width': '100px'}
+        ],
+        style_data_conditional=[
+            {
+                'if': {
+                'filter_query': '{등락률(%)} contains +',
+                'column_id': '등락률(%)'
+                },
+                'color': 'Red'
+            },
+            {
+                'if': {
+                'filter_query': '{등락률(%)} contains -',
+                'column_id': '등락률(%)'
+                },
+                'color': 'Blue'
+            },
+        ]
+    )
+
+    return history
+
+
 # Compare 수익률 비교 차트
 def get_compare_chart(period, symbol1,symbol2):
     name1 = list(etflist.loc[etflist['Symbol']==symbol1,'Name'])[0]
@@ -447,48 +496,3 @@ def get_compare_table(symbol1,symbol2):
         ],
     )
     return info_table
-
-#Search etf history 호출
-def get_history_table(symbol):
-    sql = "select 날짜, NAV, 시가, 고가, 저가, 종가, 거래량, 거래대금 from etfkor."+symbol
-    df = pd.DataFrame(get_data_from_db(sql),columns=['날짜','NAV','시가','고가','저가','종가','거래량','거래대금'])
-    df['날짜'] = df['날짜'].apply(lambda x: x.strftime('%Y-%m-%d'))
-    for col in ['시가','고가','저가','종가','거래량','거래대금']:
-        df[col] =df[col].apply(lambda x : "{:,}".format(int(x)))
-    df.sort_values('날짜',ascending=False,inplace=True)
-    history = dash_table.DataTable(
-        data=df.to_dict('records'),
-        columns=[{'id': c, 'name': c} for c in df.columns],
-        style_as_list_view=True,
-        page_size=10,
-        style_cell={'fontFamily': 'sans-serif', 'fontSize': '11px','padding':0},
-        style_data={'border':'0px'},
-        style_header={
-            'backgroundColor': 'rgb(230, 230, 230)',
-            'fontWeight': 'bold'
-        },
-        style_cell_conditional=[            
-            {'if': {'column_id': '날짜'},'textAlign': 'left','width': '200px'},
-            {'if': {'column_id': '시가총액'},'width': '100px'},
-            {'if': {'column_id': '등락률'},'width': '100px'}
-        ],
-        style_data_conditional=[
-            {
-                'if': {
-                'filter_query': '{등락률(%)} contains +',
-                'column_id': '등락률(%)'
-                },
-                'color': 'Red'
-            },
-            {
-                'if': {
-                'filter_query': '{등락률(%)} contains -',
-                'column_id': '등락률(%)'
-                },
-                'color': 'Blue'
-            },
-        ]
-    )
-
-    return history
-
